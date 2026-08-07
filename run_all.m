9a79a6b5a68a910e3328cc7fa724bc80c897bbc1
Evgenii Salganik <salganikea@gmail.com>
2026-08-07 10:55:40 +0200

    Add files via upload

--- run_all.m
+++ run_all.m
@@ -19,8 +19,8 @@ doProcessing = false;     % Reprocess raw observational datasets
 doMerge = false;          % Merge processed datasets
 doSnowMatchups = false;   % Add ERA5/MERRA2 SnowModel-LG snow products
 doStatistics = false;     % Generate manuscript statistics
-doFigures = false;        % Reproduce manuscript figures
-doSupportingInfo = true; % Generate Supporting Information figures and tables
+doFigures = true;         % Reproduce manuscript figures
+doSupportingInfo = true;  % Generate Supporting Information figures and tables
 
 %% Repository setup
 
@@ -141,6 +141,37 @@ if doStatistics
 
 end
 
+%% Check CryoSat-2 input files
+
+% Figures 3 and S5 both require the February and July CryoSat-2 files.
+if doFigures || doSupportingInfo
+
+    cs2Dir = fullfile(repoRoot, 'data', 'cs2');
+
+    febFile = fullfile(cs2Dir, ...
+        'uit_sit_v2northpolarstereo_80km_feb_2011-2023_v3p0.nc');
+
+    julFile = fullfile(cs2Dir, ...
+        'uit_sit_v2northpolarstereo_80km_jul_2011-2023_v3p0.nc');
+
+    missingFiles = {};
+
+    if ~isfile(febFile)
+        missingFiles{end+1} = febFile;
+    end
+
+    if ~isfile(julFile)
+        missingFiles{end+1} = julFile;
+    end
+
+    if ~isempty(missingFiles)
+        error('run_all:MissingCS2Files', ...
+            'Required CryoSat-2 input file(s) are missing:\n%s', ...
+            strjoin(missingFiles,newline))
+    end
+
+end
+
 %% Generate manuscript figures
 
 if doFigures
@@ -156,6 +187,7 @@ if doFigures
 
     analyze_density_model(repoRoot)
     analyze_snow_dependence(repoRoot)
+    make_figure3(repoRoot)
 
 end
 
@@ -164,10 +196,12 @@ end
 if doSupportingInfo
 
     make_cross_validation_table(repoRoot)
+    empirical_density_uncertainty(repoRoot)
     make_figureS1_mosaic_coring_density_figure(repoRoot)
     make_figureS2_ridge_density_figure(repoRoot)
     make_figureS3_snow_dependence_merra(repoRoot)
     make_figureS4_density_formulations(repoRoot)
+    make_figureS5(repoRoot)
     make_figureS6_snow_model_evaluation(repoRoot)
 
 end
