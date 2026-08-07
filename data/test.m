e3edfb3e99e0f03b1b764a65677899db766026a4
Evgenii Salganik <salganikea@gmail.com>
2026-08-07 19:26:37 +0200

    Delete data/test.m

--- data/test.m
+++
@@ -1,6 +0,0 @@
-repoRoot = 'C:\Users\evsalg001\Documents\MATLAB\Density parametrization\script';
-
-addpath(genpath(fullfile(repoRoot,'scripts')))
-addpath(genpath(fullfile(repoRoot,'external')))
-
-make_si_ridge_density_figure(repoRoot)
